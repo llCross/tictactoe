56f65b5288c78676dc242a3afced68e61e718718
Cross <118707038+llCross@users.noreply.github.com>
2023-11-18 21:55:29 +0800

    Update game logic and clean code

--- script.js
+++ script.js
@@ -1,45 +1,107 @@
 const gameboard = (function (){
 
-    let currentPlayer
+    let gameboard = ["", "", "", "", "", "", "", "", ""]    //game.board array
 
-    const playerTurn = () => {
-        currentPlayer = !currentPlayer
+    const getGameBoard = () => gameboard;                   //safely get gameboard
+
+    const update = (cellIndex, currentPlayer) => {          //updates gameboard
+        if (getRunning() = true) {
+        gameboard[cellIndex] = currentPlayer;
+        } else {
+            gameboard = ["", "", "", "", "", "", "", "", ""]
         }
-    
-    const handleClick = (e) => {
-        console.log(index)
-        const cell = e.target
-        const playerMark = currentPlayer ? cell.textContent = 'o' : cell.textContent = 'x';
-        playerTurn();
     }
 
     return {
-        handleClick
+        update,
+        getGameBoard,
     }
 })();
 
 
 const game = (function () {
 
+    let currentPlayer = "x";
+    let running = false;
+    const getRunning = () => running;
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    const boardElements = document.querySelectorAll('.cell');           //grabs cells
+    const boardContainer = document.querySelector('.cell-container');   //grabs cell container
+    const startButton = document.querySelector('.start-button');        //grabs start button
+
     const start = () => {
-        startButton.parentNode.removeChild(startButton);
-        const boardElements = document.querySelectorAll('.cell');
-        const boardContainer = document.querySelector('.cell-container');
+        running = true;
+        startButton.parentNode.removeChild(startButton);                //removes start button
+        
         boardElements.forEach(cell => {
         cell.classList.add('square')
         })
-        boardContainer.classList.add('gameboard')
+        boardContainer.classList.add('gameboard')                       //adds css to cells to be visible
 
         boardElements.forEach(cell => {
-        cell.addEventListener('click', gameboard.handleClick, {once: true})
+        cell.addEventListener('click', handleClick, {once: true})        //adds event listener
     })
     }
 
-    const startButton = document.querySelector('.start-button');
-    startButton.addEventListener('click', start)
+    const handleClick = (e) => {                                        //handles click and renders
+        const cell = e.target
+        const cellIndex = parseInt(event.target.id);
+        cell.textContent = currentPlayer;
+        gameboard.update(cellIndex, currentPlayer);
+        playerTurn();
+        checkWin();
+    }
 
-//game.restart
+    const playerTurn = () => {                                          //switches player turn
+        currentPlayer = (currentPlayer == "x") ? "o" : "x";
+        }
+
+    const checkWin = () => {                                            //checks for win or draw
+        let roundWon = false
+
+        for(let i = 0; i < winConditions.length; i++){
+            const condition = winConditions[i];
+            const cellA = gameboard.getGameBoard()[condition[0]];
+            const cellB = gameboard.getGameBoard()[condition[1]];
+            const cellC = gameboard.getGameBoard()[condition[2]];
+    
+            if(cellA == "" || cellB == "" || cellC == ""){
+                continue;
+            }
+            if(cellA == cellB && cellB == cellC){
+                roundWon = true;
+                break;
+            }
+        }
+        if (roundWon) {
+            console.log('win');
+            running = false
+        } else if (!gameboard.getGameBoard().includes('')){
+            console.log('draw')
+            running = false
+        }
+    }
+
+    const restart = () => {
+        gameboard.update()
+        boardElements.forEach(cell => cell.textContent = "");
+    }
+    //event listeners
+    startButton.addEventListener('click', start)                         //start button event listener
+    restartButton.addEventListener('click', restart)                    //restart button event listener
+
 return {
+    handleClick,
+    getRunning,
 }
 
 })();
